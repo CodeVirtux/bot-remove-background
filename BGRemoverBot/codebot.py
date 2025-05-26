290d8d432b38f4e492aab81fcdf50c4f7220526f
Abdellah Ait-si <abdellah.aitsi.dev@gmail.com>
2025-05-26 14:12:24 +0100

    Update codebot.py

--- BGRemoverBot/codebot.py
+++ BGRemoverBot/codebot.py
@@ -5,8 +5,8 @@ from PIL import Image
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
-TOKEN = "8138120207:AAE-lDPK0ZyLGAmawcF-g-_T9xXrYpduEDQ"
-# Load the ONNX model once
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+# Load the ONNX model once  
 session = new_session("isnet-general.onnx")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
